6b3036a9afd6d7445a7283ba1d4c15271922269b
inferno47 <aymericgand@gmail.com>
2017-11-04 16:06:10 +0100

    .gitignore

---
+++ platforms/android/assets/www/js/Accelerometer.js
@@ -0,0 +1,42 @@
+﻿/*
+ * ACCELEROMETER API
+*/
+
+// Start Accelerometer API
+function startAccelerometer() {
+    var options = { frequency: 100 };
+    watchAccelerometerID = navigator.accelerometer.watchAcceleration(onAccelerometerSuccess, onAccelerometerError, options);
+}
+
+// Stop Accelerometer API
+function stopAccelerometer() {
+    if (watchAccelerometerID) {
+        navigator.accelerometer.clearWatch(watchAccelerometerID);
+        watchAccelerometerID = null;
+    }
+}
+
+// onSuccess: Get current accelerometer values
+function onAccelerometerSuccess(acceleration) {
+    // for debug purpose to print out accelerometer values
+    var element = document.getElementById('accelerometer');
+    element.innerHTML = 'Acceleration X: ' + acceleration.x + '<br />' +
+        'Acceleration Y: ' + acceleration.y + '<br />' +
+        'Acceleration Z: ' + acceleration.z;
+    if (acceleration.y > 7) {
+        $("#arView").fadeIn();
+        $("#topView").hide();
+        // document.getElementById('body').style.background = "#d22";
+        document.getElementById('body').style.background = "transparent";
+    } else {
+        $("#arView").hide();
+        $("#topView").fadeIn();
+        document.getElementById('body').style.background = "#fff";
+    }
+}
+
+// onError: Failed to get the acceleration
+function onAccelerometerError(accelerometerError) {
+    alert("Accelerometer initialization failed");
+    document.getElementById('log').innerHTML += "onError." + accelerometerError.code;
+}
---
+++ platforms/android/assets/www/js/Index.js
@@ -0,0 +1,22 @@
+﻿function toggleView() {
+    if ($(".listView").is(":visible")) {
+        $(".listView").hide();
+        $("#map").height($(window).height() - 60);
+        $(".mapView").fadeIn(
+            function () {
+                google.maps.event.trigger(map, "resize");
+                map.fitBounds(bounds);
+            });
+        $("#viewbtn").html("Setup");
+    }
+    else if ($(".mapView").is(":visible")) {
+        $(".mapView").hide();
+        $(".SetupView").fadeIn();
+        $("#viewbtn").html("List");
+    }
+    else {
+        $(".SetupView").hide();
+        $(".listView").fadeIn();
+        $("#viewbtn").html("Map");
+    }
+}
