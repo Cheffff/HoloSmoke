3cd3cbd5135f5b9e91a525f590fb0fd1800af11e
inferno47 <aymericgand@gmail.com>
2017-11-04 16:31:21 +0100

    ToogleView
    include index.js in index.html

--- platforms/android/assets/www/js/Index.js
+++ platforms/android/assets/www/js/Index.js
@@ -1,27 +1,4 @@
-﻿/*function toggleView() {
-    if ($(".listView").is(":visible")) {
-        $(".listView").hide();
-        $("#map").height($(window).height() - 60);
-        $(".mapView").fadeIn(
-            function () {
-                google.maps.event.trigger(map, "resize");
-                map.fitBounds(bounds);
-            });
-        $("#viewbtn").html("List");
-    }
-    else if ($(".mapView").is(":visible")) {
-        $(".mapView").hide();
-        $(".SetupView").fadeIn();
-        $("#viewbtn").html("List");
-    }
-    else {
-        $(".SetupView").hide();
-        $(".listView").fadeIn();
-        $("#viewbtn").html("Map");
-    }
-}*/
-
-function toggleView() {
+﻿function toggleView() {
     if ($(".listView").is(":visible")) {
         $(".listView").hide();
         $("#map").height($(window).height() - 60);
@@ -37,3 +14,28 @@ function toggleView() {
         $("#viewbtn").html("Map");
     }
 }
+
+/*function toggleView() {
+    if ($(".listView").is(":visible")) {
+        $(".listView").hide();
+        $(".SetupView").hide();
+        $("#map").height($(window).height() - 60);
+        $(".mapView").fadeIn(
+            function () {
+                google.maps.event.trigger(map, "resize");
+                map.fitBounds(bounds);
+        });
+        $("#viewbtn").html("Setup");
+    }
+    else if ($(".mapView").is(":visible")) {
+        $(".mapView").hide();
+        $(".listView").hide();
+        $(".SetupView").fadeIn();
+        $("#viewbtn").html("List");
+    }
+    else {
+        $(".SetupView").hide();
+        $(".listView").fadeIn();
+        $("#viewbtn").html("Map");
+    }
+}*/
