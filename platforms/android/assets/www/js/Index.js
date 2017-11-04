914488aba5ed55d48d512535f032f7c0377b5386
inferno47 <aymericgand@gmail.com>
2017-11-04 16:28:32 +0100

    ToogleView Ok

--- platforms/android/assets/www/js/Index.js
+++ platforms/android/assets/www/js/Index.js
@@ -1,4 +1,4 @@
-﻿function toggleView() {
+﻿/*function toggleView() {
     if ($(".listView").is(":visible")) {
         $(".listView").hide();
         $("#map").height($(window).height() - 60);
@@ -7,7 +7,7 @@
                 google.maps.event.trigger(map, "resize");
                 map.fitBounds(bounds);
             });
-        $("#viewbtn").html("Setup");
+        $("#viewbtn").html("List");
     }
     else if ($(".mapView").is(":visible")) {
         $(".mapView").hide();
@@ -16,6 +16,23 @@
     }
     else {
         $(".SetupView").hide();
+        $(".listView").fadeIn();
+        $("#viewbtn").html("Map");
+    }
+}*/
+
+function toggleView() {
+    if ($(".listView").is(":visible")) {
+        $(".listView").hide();
+        $("#map").height($(window).height() - 60);
+        $(".mapView").fadeIn(
+            function () {
+                google.maps.event.trigger(map, "resize");
+                map.fitBounds(bounds);
+            });
+        $("#viewbtn").html("List");
+    } else {
+        $(".mapView").hide();
         $(".listView").fadeIn();
         $("#viewbtn").html("Map");
     }
